e17b60003dfb4c8c5f55bb83bb794d4bfeb23474
zsystem-jmeneguzzi <juan@MBP-de-Lisandro.fibertel.com.ar>
2026-05-21 20:32:20 -0300

    v1.1

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,4 @@
-print("Hello World!") # Esta línea de código imprime la cadena de texto "Hello World!" en la salida estándar (la consola)
+print("Hello World!")
+# Esta línea de código imprime la cadena de texto "Hello World!" en la salida estándar (la consola)
+# Esta línea de código imprime la cadena de texto "Hello World!" en la salida estándar (la consola)
+# Esta línea de código imprime la cadena de texto "Hello World!" en la salida estándar (la consola)
